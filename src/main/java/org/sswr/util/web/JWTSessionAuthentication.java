f969f19ce222a11f695b3114f30b1b6ae81b7297
Simon Wong <sswroom@yahoo.com>
2021-06-11 00:14:44 +0800

    Added JWTSessionFilter

---
+++ src/main/java/org/sswr/util/web/JWTSessionAuthentication.java
@@ -0,0 +1,71 @@
+package org.sswr.util.web;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.sswr.util.data.DataTools;
+
+public class JWTSessionAuthentication implements Authentication
+{
+	private JWTSession sess;
+	private List<SimpleGrantedAuthority> authorities;
+
+	public JWTSessionAuthentication(JWTSession sess)
+	{
+		this.sess = sess;
+		List<String> roles = this.sess.getRoleList();
+		int i = 0;
+		int j = roles.size();
+		this.authorities = new ArrayList<SimpleGrantedAuthority>();
+		while (i < j)
+		{
+			this.authorities.add(new SimpleGrantedAuthority(roles.get(i)));
+			i++;
+		}
+	}
+
+	@Override
+	public String getName() {
+		return this.sess.getUserName();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public Object getCredentials() {
+		return null;
+	}
+
+	@Override
+	public Object getDetails() {
+		return null;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		return this.sess;
+	}
+
+
+	@Override
+	public boolean isAuthenticated() {
+		return true;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+	}
+
+	@Override
+	public String toString()
+	{
+		return DataTools.toObjectString(this.sess);
+	}
+}
--- src/main/java/org/sswr/util/web/JWTSessionFilter.java
+++ src/main/java/org/sswr/util/web/JWTSessionFilter.java
@@ -1,26 +1,60 @@
 package org.sswr.util.web;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
-@Component
+
 public class JWTSessionFilter extends GenericFilterBean
 {
-	public JWTSessionFilter()
+	private JWTSessionManager sessMgr;
+	public JWTSessionFilter(JWTSessionManager sessMgr)
 	{
 		super();
+		this.sessMgr = sessMgr;
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
 	{
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		if (authentication == null || !authentication.getClass().equals(JWTSessionAuthentication.class))
+		{
+			if (request instanceof HttpServletRequest)
+			{
+				HttpServletRequest req = (HttpServletRequest)request;
+				JWTSession sess = null;
+				String tokenHdr = req.getHeader("X-Token");
+				if (tokenHdr != null)
+				{
+					sess = this.sessMgr.getSession(tokenHdr);
+				}
+				if (sess == null)
+				{
+					Map<String, Object> params = HttpUtil.parseParams(req, null);
+					Object token = params.get("Token");
+					if (token != null)
+					{
+						sess = this.sessMgr.getSession(token.toString());
+					}
+				}
+				if (sess != null)
+				{
+					securityContext.setAuthentication(new JWTSessionAuthentication(sess));
+				}
+			}
+		}
 		chain.doFilter(request, response);
 	}
 }
