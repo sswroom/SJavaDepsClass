3201ee541a16bf693de2f5355d24bc8ea6aa89f1
Simon Wong <sswroom@yahoo.com>
2022-06-02 21:25:02 +0800

    Enhance Attachment handling

--- src/main/java/org/sswr/util/net/email/EmailUtil.java
+++ src/main/java/org/sswr/util/net/email/EmailUtil.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.PrivateKey;
 import java.security.Security;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.BodyPart;
@@ -17,18 +21,32 @@ import javax.mail.Header;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.cms.AttributeTable;
+import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
+import org.bouncycastle.asn1.smime.SMIMECapabilitiesAttribute;
+import org.bouncycastle.asn1.smime.SMIMECapability;
+import org.bouncycastle.asn1.smime.SMIMECapabilityVector;
+import org.bouncycastle.asn1.smime.SMIMEEncryptionKeyPreferenceAttribute;
+import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaCertStore;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cms.CMSException;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.cms.SignerInformationStore;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoGeneratorBuilder;
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.mail.smime.CMSProcessableBodyPartInbound;
+import org.bouncycastle.mail.smime.SMIMEException;
+import org.bouncycastle.mail.smime.SMIMESignedGenerator;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.util.Selector;
 import org.bouncycastle.util.Store;
@@ -73,6 +91,64 @@ public class EmailUtil
 		return false;
 	}
 
+	private static void setEmailContent(ReceivedEmail email, Part content) throws IOException, MessagingException
+	{
+		if (content.isMimeType("multipart/signed"))
+		{
+			Multipart mpart = (Multipart)content.getContent();
+			int i = 0;
+			int j = mpart.getCount();
+			while (i < j)
+			{
+				BodyPart part = mpart.getBodyPart(i);
+				if (part.isMimeType("application/pkcs7-signature"))
+				{
+					String fileName = part.getFileName();
+					if (fileName.equals("smime.p7s"))
+					{
+						byte[] signature = part.getInputStream().readAllBytes();
+						email.setSignVerified(verifySign(mpart.getBodyPart(0), signature));
+					}
+					else
+					{
+						/////////////////////////////////////////////
+						System.out.println("Unknown Signature file");
+					}
+				}
+				else
+				{
+					setEmailContent(email, part);
+				}
+				i++;					
+			}
+		}
+		else if (content.isMimeType("multipart/*"))
+		{
+			Multipart mpart = (Multipart)content.getContent();
+			int i = 0;
+			int j = mpart.getCount();
+			while (i < j)
+			{
+				BodyPart part = mpart.getBodyPart(i);
+				String disposition = part.getDisposition();
+				if (disposition == null)
+				{
+					setEmailContent(email, part);
+				}
+				else
+				{
+					email.addAttachment(disposition.equals("inline"), part.getFileName(), part.getContentType(), part.getInputStream().readAllBytes());
+				}
+				i++;					
+			}
+		}
+		else
+		{
+			email.setContent(content.getInputStream().readAllBytes());
+			email.setContentType(content.getContentType());
+		}
+	}
+
 	public static ReceivedEmail toReceivedEmail(Message msg)
 	{
 		ReceivedEmail email = new ReceivedEmail();
@@ -85,58 +161,7 @@ public class EmailUtil
 				header = (Header)enumer.nextElement();
 				email.addHeader(header.getName(), header.getValue());
 			}
-			if (msg.isMimeType("multipart/signed"))
-			{
-				Multipart mpart = (Multipart)msg.getContent();
-				int i = 0;
-				int j = mpart.getCount();
-				while (i < j)
-				{
-					BodyPart part = mpart.getBodyPart(i);
-					if (part.isMimeType("application/pkcs7-signature"))
-					{
-						String fileName = part.getFileName();
-						if (fileName.equals("smime.p7s"))
-						{
-							byte[] signature = part.getInputStream().readAllBytes();
-							email.setSignVerified(verifySign(mpart.getBodyPart(0), signature));
-						}
-						else
-						{
-							/////////////////////////////////////////////
-							System.out.println("Unknown Signature file");
-						}
-					}
-					else
-					{
-						String disposition = part.getDisposition();
-						if (disposition == null)
-						{
-							email.setContent(part.getInputStream().readAllBytes());
-							email.setContentType(part.getContentType());
-						}
-						else
-						{
-							System.out.println("Content Type: "+part.getContentType());
-							System.out.println("Description: "+part.getDescription());
-							System.out.println("Disposition: "+disposition);
-							System.out.println("FileName: "+part.getFileName());
-							System.out.println("Size: "+part.getSize());
-							System.out.println("----------------");
-							/////////////////////////////////////////////
-						}
-					}
-					i++;					
-				}
-			}
-			else if (msg.isMimeType("multipart/*"))
-			{
-				/////////////////////////////////////////////
-			}
-			else
-			{
-				email.setContent(msg.getInputStream().readAllBytes());
-			}
+			setEmailContent(email, msg);
 			return email;
 		}
 		catch (MessagingException ex)
@@ -194,6 +219,67 @@ public class EmailUtil
 		{
 			ex.printStackTrace();
 			return false;
+		}
+	}
+
+	public static MimeMessage signEmail(MimeMessage msg, X509Certificate cert, PrivateKey key)
+	{		
+		Properties props = System.getProperties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		SMIMECapabilityVector capabilities = new SMIMECapabilityVector();
+		capabilities.addCapability(SMIMECapability.dES_EDE3_CBC);
+		capabilities.addCapability(SMIMECapability.rC2_CBC, 128);
+		capabilities.addCapability(SMIMECapability.dES_CBC);
+		
+		ASN1EncodableVector attributes = new ASN1EncodableVector();
+		attributes.add(new SMIMEEncryptionKeyPreferenceAttribute(
+				new IssuerAndSerialNumber(
+						new X500Name(cert.getIssuerDN().getName()),
+								cert.getSerialNumber())));
+		attributes.add(new SMIMECapabilitiesAttribute(capabilities));
+		
+		try
+		{
+			SMIMESignedGenerator signer = new SMIMESignedGenerator();
+			signer.addSignerInfoGenerator(new JcaSimpleSignerInfoGeneratorBuilder().setProvider(BouncyCastleProvider.PROVIDER_NAME)
+					.setSignedAttributeGenerator(new AttributeTable(attributes)).build("SHA1withRSA", key, cert));
+
+			List<X509Certificate> certList = new ArrayList<X509Certificate>();
+			certList.add(cert);
+			Store<?> certs = new JcaCertStore(certList);
+			signer.addCertificates(certs);
+			
+			MimeMultipart mm = signer.generate(msg);
+			MimeMessage signedMessage = new MimeMessage(session);
+			
+			Enumeration<?> headers = msg.getAllHeaderLines();
+			while (headers.hasMoreElements()) {
+				signedMessage.addHeaderLine((String)headers.nextElement());
+			}
+			signedMessage.setContent(mm);
+			signedMessage.saveChanges();
+			return signedMessage;
+		}
+		catch (MessagingException ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+		catch (CertificateEncodingException ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+		catch (SMIMEException ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+		catch (OperatorCreationException ex)
+		{
+			ex.printStackTrace();
+			return null;
 		}
 	}
 }
